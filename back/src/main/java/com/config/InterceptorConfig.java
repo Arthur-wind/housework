3be1d0e3e65b7ac44f3ea16b8c348f54b3d4a211
Lex <1456511871@qq.com>
2025-06-18 21:57:08 +0800

    修改登录功能添加跳转携带token

--- back/src/main/java/com/config/InterceptorConfig.java
+++ back/src/main/java/com/config/InterceptorConfig.java
@@ -6,38 +6,45 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
-import com.interceptor.AuthorizationInterceptor;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class InterceptorConfig extends WebMvcConfigurationSupport{
-	
+public class InterceptorConfig implements WebMvcConfigurer {
+
 	@Bean
-    public AuthorizationInterceptor getAuthorizationInterceptor() {
-        return new AuthorizationInterceptor();
-    }
+	public AuthorizationInterceptor getAuthorizationInterceptor() {
+		return new AuthorizationInterceptor();
+	}
 
 	@Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(getAuthorizationInterceptor()).
-				addPathPatterns("/**").excludePathPatterns(
-            "/employer/register",
-            "/employee/register",
-            "/login",
-            "/static/**"
-        );
-//        super.addInterceptors(registry);
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:8081")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.exposedHeaders("Authorization")
+				.allowedHeaders("*")
+				.allowCredentials(true);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(getAuthorizationInterceptor())
+				.addPathPatterns("/**")
+				.excludePathPatterns(
+						"/employer/register",
+						"/employee/register",
+						"/login",
+						"/static/**"
+				);
 	}
 
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**")
-        .addResourceLocations("classpath:/resources/")
-        .addResourceLocations("classpath:/static/")
-        .addResourceLocations("classpath:/admin/")
-        .addResourceLocations("classpath:/front/")
-        .addResourceLocations("classpath:/public/");
-		super.addResourceHandlers(registry);
-    }
+				.addResourceLocations("classpath:/resources/")
+				.addResourceLocations("classpath:/static/")
+				.addResourceLocations("classpath:/admin/")
+				.addResourceLocations("classpath:/front/")
+				.addResourceLocations("classpath:/public/");
+	}
 }
